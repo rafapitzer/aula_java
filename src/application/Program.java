7738f2bedf0e7e779fc1994bbf7d69ee7349bc31
rafapitzer <rafa_thadeupitzer@hotmail.com>
2018-11-16 19:28:15 -0200

    inserido mensagem boa noite

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,8 +3,10 @@ package application;
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("bom dia");
-		System.out.println("boa tarde");
+		
+		System.out.println("bom dia!");
+		System.out.println("boa tarde!");
+		System.out.println("boa noite!");
+		
 	}
-
 }
